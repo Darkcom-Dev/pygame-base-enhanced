cc0b8702aabe946d9338721935ae74a387809e09
Braulio Madrid <brauliomadrid.developer@gmail.com>
2023-04-20 07:37:18 -0500

    scene_title.py refactorization

--- scene_title.py
+++ scene_title.py
@@ -41,9 +41,6 @@ class SceneTitle(scene.Scene):
 		
 		self.background = pygame.image.load(cfg.SpritesDir + cfg.ControllerBackground).convert()
 		
-		# ~ pygame.joystick.init() # inicializa el modulo de joystick
-		# ~ self.J1 = pygame.joystick.Joystick(0) # crea una instancia de un joystick
-		# ~ self.J1.init() # inicializa la instancia de un joystick
 		
 		self.info_text_list = list()
 		self.hat_list = list()
@@ -60,19 +57,6 @@ class SceneTitle(scene.Scene):
 				self.hat_list.append(font.Font(cfg.ParagraphFont, cfg.P2,f'info {i}',"tl",(100,10 + (20*i)),cfg.DefaultColor,cfg.InfoOutlineColor))
 		
 
-		self.buttonBPosition = (314,382)
-		self.buttonAPosition = (285,406)
-		self.buttonXPosition = (256,382)
-		self.buttonYPosition = (285,358)
-		
-		self.buttonLPosition = (95,290,10,10)
-		self.buttonRPosition = (280,290,10,10)
-		
-		self.dPadUp = (90,360,10,10)
-		self.dPadDown = (90,396,10,10)
-		self.dPadLeft = (70,378,10,10)
-		self.dPadRight = (108,378,10,10)
-		
 		self.activeColor = (255,0,0)
 		self.normalColor = (128, 128, 128)
 		
@@ -94,67 +78,27 @@ class SceneTitle(scene.Scene):
 		self.history_text.text = f'{str(self.mouse)}'
 		
 	def on_event(self):
-
-		# Directions
-		if self.dir.horizontal == -1:
-			self.colorLeft = self.activeColor
-			self.event_text.text = "Key_left"
-		else:
-			self.colorLeft = self.normalColor
-		if self.dir.horizontal == 1:
-			self.colorRight = self.activeColor
-			self.event_text.text = "Key_right"
-		else:
-			self.colorRight = self.normalColor
-		if self.dir.vertical == 1:
-			self.colorUp = self.activeColor
-			self.event_text.text = "Key_up"
-		else:
-			self.colorUp = self.normalColor
-		if self.dir.vertical == -1:
-			self.colorDown = self.activeColor
-			self.event_text.text = "Key_down"
-		else:
-			self.colorDown = self.normalColor
+		
 		# Buttons
-		if self.dir.button == 'AB':
-			self.event_text.text == 'AB'
-		if self.dir.button == 'XY':
-			self.event_text.text == 'XY'
-		if self.dir.button == 'LR':
-			self.event_text.text == 'LR'
-		if self.dir.button == 'A':
-			self.colorA = self.activeColor
-			self.event_text.text = "Key_space or A"
-		else:
-			self.colorA = self.normalColor
-		if self.dir.button == 'B':
-			self.colorB = self.activeColor
-			self.event_text.text = "Key_left_ctrl or B"
-		else:
-			self.colorB = self.normalColor
-		if self.dir.button == 'X':
-			self.colorX = self.activeColor
-			self.event_text.text = "Key_left_shift or X"
-		else:
-			self.colorX = self.normalColor
-		if self.dir.button == 'Y':
-			self.colorY = self.activeColor
-			self.event_text.text = "Key_left_alt or Y"
-		else:
-			self.colorY = self.normalColor
-		if self.dir.keys[pygame.K_TAB]:
-			self.event_text.text = "Key_Tab"
-		if self.dir.button == 'L':
-			self.colorL = self.activeColor
-			self.event_text.text = 'L'
-		else:
-			self.colorL = self.normalColor
-		if self.dir.button == 'R':
-			self.colorR = self.activeColor
-			self.event_text.text = 'R'
-		else:
-			self.colorR = self.normalColor
+		self.colorLeft = self.activeColor if self.dir.horizontal == -1 else self.normalColor
+		self.colorRight = self.activeColor if self.dir.horizontal == 1 else self.normalColor
+		self.colorUp = self.activeColor if self.dir.vertical == 1 else self.normalColor
+		self.colorDown = self.activeColor if self.dir.vertical == -1 else self.normalColor
+		self.colorA = self.activeColor if self.dir.button == 'A' or self.dir.button == 'AB' else self.normalColor
+		self.colorB = self.activeColor if self.dir.button == 'B' or self.dir.button == 'AB' else self.normalColor
+		self.colorX = self.activeColor if self.dir.button == 'X' or self.dir.button == 'XY' else self.normalColor
+		self.colorY = self.activeColor if self.dir.button == 'Y' or self.dir.button == 'XY' else self.normalColor
+		self.colorL = self.activeColor if self.dir.button == 'L' or self.dir.button == 'LR' else self.normalColor
+		self.colorR = self.activeColor if self.dir.button == 'R' or self.dir.button == 'LR' else self.normalColor
+
+		if self.dir.horizontal == -1:self.event_text.text = "Key_left"		
+		if self.dir.horizontal == 1:self.event_text.text = "Key_right"
+		if self.dir.vertical == 1:self.event_text.text = "Key_up"
+		if self.dir.vertical == -1:	self.event_text.text = "Key_down"
+
+		self.tag_buttons()
+		
+		
 		if self.dir.J1:
 			for i in range(self.dir.J1.get_numbuttons()):
 				if self.dir.J1.get_button(i):
@@ -172,17 +116,9 @@ class SceneTitle(scene.Scene):
 		self.event_text.position = (math.sin(pygame.time.get_ticks()/1000) * cfg.WIDTH/4 + cfg.WIDTH/2, random.randrange((cfg.HEIGHT/2) -2,(cfg.HEIGHT/2) +2))
 		self.event_text.draw(screen, "chromatic",2)
 		
-		pygame.draw.circle(screen,self.colorA,self.buttonAPosition,10)
-		pygame.draw.circle(screen,self.colorB,self.buttonBPosition,10)
-		pygame.draw.circle(screen,self.colorX,self.buttonXPosition,10)
-		pygame.draw.circle(screen,self.colorY,self.buttonYPosition,10)
+		self.draw_circle_buttons(screen)
 		
-		pygame.draw.rect(screen,self.colorR, self.buttonRPosition)
-		pygame.draw.rect(screen,self.colorL, self.buttonLPosition)
-		pygame.draw.rect(screen,self.colorUp, self.dPadUp)
-		pygame.draw.rect(screen,self.colorDown, self.dPadDown)
-		pygame.draw.rect(screen,self.colorLeft, self.dPadLeft)
-		pygame.draw.rect(screen,self.colorRight, self.dPadRight)
+		self.draw_rectangle_buttons(screen)
 		
 		self.history_text.draw(screen)
 		
@@ -194,6 +130,44 @@ class SceneTitle(scene.Scene):
 		
 		pygame.display.flip()
 
+	def tag_buttons(self):
+		button_tags = {
+			'A':'Key_space or A',
+			'B':'Key_left_ctrl or B',
+			'X':'Key_left_shift or X',
+			'Y':'Key_left_alt or Y',
+			'L':'Key_Q or L',
+			'R':'Key_E or R',
+			'AB':'AB',
+			'XY':'XY',
+			'LR':'LR'
+		}
+
+		for tag in button_tags:
+			if self.dir.button == tag:
+				self.event_text.text = button_tags[tag]
+
+	def draw_circle_buttons(self, screen):
+		circles = [
+			((314,382),self.colorA),
+			((285,406),self.colorB),
+			((256,382),self.colorX),
+			((285,358),self.colorY)
+		]
+		for circle in circles:
+			pygame.draw.circle(screen,circle[1],circle[0],10)
+
+	def draw_rectangle_buttons(self, screen):
+		rectangles = [
+			((280,290,10,10),self.colorR),
+			((95,290,10,10),self.colorL),
+			((90,360,10,10),self.colorUp),
+			((90,396,10,10),self.colorDown),
+			((70,378,10,10),self.colorLeft),
+			((108,378,10,10),self.colorRight)
+		]
+		for rectangle in rectangles:
+			pygame.draw.rect(screen,rectangle[1],rectangle[0])
 # ---------------------------------------------------------------------
 
 def main():
